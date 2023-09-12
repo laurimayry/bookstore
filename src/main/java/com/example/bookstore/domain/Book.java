a656d5e1201705ecb32193c9bc9c9efd24fd62da
Lauri Mäyry <lauri.mayry@gmail.com>
2023-09-12 11:20:02 +0300

    link to database

--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -1,16 +1,23 @@
 package com.example.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
 	private String title;
 	private String author;
 	private long publicationYear;
 	private long isbn;
 	private long price;
 
-	public Book() {
-		super();
-	}
+	 protected Book() {}
 	
 	public Book(String title, String author, long publicationYear, long isbn, long price) {
 		super();
@@ -20,6 +27,15 @@ public class Book {
 		this.isbn = isbn;
 		this.price = price;
 	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	public String getTitle() {
 		return title;
@@ -59,6 +75,12 @@ public class Book {
 
 	public void setPrice(long price) {
 		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ",title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
 	
 	
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,18 +1,27 @@
 package com.example.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 
 
 @Controller
-@ResponseBody
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository; 
 
 	
-	@RequestMapping("index")
-	public String bookController() {
-		return "Book";
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+		public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+
+		return "booklist";
 	}
 }
